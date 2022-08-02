4a2c6ac418f2002ef42e3d91de9c23ea30eb3630
Виктор <viktor@MacBook-Air-Viktor.local>
2022-08-02 19:46:53 +0300

    --------

--- server/controllers/adminChangeController.js
+++ server/controllers/adminChangeController.js
@@ -12,23 +12,23 @@ class AdminChangeController {
 
 	async editOrder(req, res, next) {
 		try {
-			// const data = req.body
-			// for (let i of data) {
-			// const index = data.indexOf(i)
-			// await models.AdminArticle.update({ idx: index }, { where: { id: i.id } })
-			const art = await models.AdminArticle.findAll({ where: { idx: 1 } })
-			// if (art) {
-			// 	art.idx = 5
+			const data = req.body
+			for (let i of data) {
+				const index = data.indexOf(i)
+				await models.AdminArticle.update({ idx: index }, { where: { id: i.id } })
+				// const art = await models.AdminArticle.findAll({ where: { idx: 1 } })
+				// if (art) {
+				// 	art.idx = 5
 
-			// 	await art.save()
-			// } else {
-			// 	console.log('тут ошибк')
-			// }
+				// 	await art.save()
+				// } else {
+				// 	console.log('тут ошибк')
+				// }
 
-			// }
+			}
 
-			// return res.json({ message: `Сохранено успешно` })
-			return res.json(art)
+			return res.json({ message: `Сохранено успешно` })
+			// return res.json(art)
 		}
 		catch (e) {
 			next(ApiError.badRequest(e.message))
