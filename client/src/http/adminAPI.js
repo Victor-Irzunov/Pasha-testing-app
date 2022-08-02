8579676e6a2bcafa67eeed37a92cd50a6dbf886a
Виктор <viktor@MacBook-Air-Viktor.local>
2022-08-02 21:55:36 +0300

    ----

--- client/src/http/adminAPI.js
+++ client/src/http/adminAPI.js
@@ -25,6 +25,6 @@ export const editOneArticle = async (obj) => {
 	return data
 }
 export const changeOrderArticles = async (obj) => {
-	const { data } = await $authHost.put('api/admin/change/order', { a: JSON.stringify(obj) })
+	const { data } = await $authHost.post('api/admin/change/order', { a: JSON.stringify(obj) })
 	return data
 }
