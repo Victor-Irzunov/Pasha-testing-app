2806423e53845525e8c120b0bc92016bf0873b24
Виктор <viktor@MacBook-Air-Viktor.local>
2022-08-16 16:11:39 +0300

    helmet false

--- server/index.js
+++ server/index.js
@@ -27,7 +27,9 @@ const __dirname = path.dirname(__filename)
 
 
 app.use(cors())
-app.use(helmet())
+app.use(helmet({
+	contentSecurityPolicy: false,
+}))
 app.use(express.json())
 app.use(express.static('static'))
 app.use(fileUpload({}))
