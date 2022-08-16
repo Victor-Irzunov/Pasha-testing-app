becb263e0c3e115a5f952a519f83e6343a6a5b96
Виктор <viktor@MacBook-Air-Viktor.local>
2022-08-16 16:30:33 +0300

    helmet policy: cross-origin

--- server/index.js
+++ server/index.js
@@ -27,19 +27,17 @@ const __dirname = path.dirname(__filename)
 
 
 app.use(cors())
-app.use(helmet({
-	contentSecurityPolicy: false,
-}))
+app.use(helmet(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })))
 app.use(express.json())
 app.use(express.static('static'))
 app.use(fileUpload({}))
 app.use(compression())
 app.use('/api', router)
 
-app.all('/*', function (req, res, next) {
-	res.header("Cross-Origin-Resource-Policy", "cross-origin");
-	next();
-});
+// app.all('/*', function (req, res, next) {
+// 	res.header("Cross-Origin-Resource-Policy", "cross-origin");
+// 	next();
+// });
 
 // app.use(
 // 	helmet.contentSecurityPolicy({
